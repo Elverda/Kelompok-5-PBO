c83f9e9b803ad42a2be304440ffe2be8895e5ce2
Izora Elverda <izoraelverda117@gmail.com>
2025-05-04 22:55:37 +0700

    Add files via upload

--- belajarspring-main/src/main/java/com/example/belajarspring/BelajarController.java
+++ belajarspring-main/src/main/java/com/example/belajarspring/BelajarController.java
@@ -1,24 +1,20 @@
 package com.example.belajarspring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BelajarController {
 
     @GetMapping("/")
     public String hello() {
-        return "welcome";
+        return "Hello World";
     }
 
-    @GetMapping("/hello/(name)")
-    public String helloName (@PathVariable String name) {
-        return "Hello" + name;
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello " + name;
     }
 
-    @GetMapping("/welcome")
-    public String showAddForm(){
-        return "welcome";
-    }
 }
