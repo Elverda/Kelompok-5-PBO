874268952914c06b0d10e6ebecae4b3c508f1daa
Izora Elverda <izoraelverda117@gmail.com>
2025-05-22 15:49:08 +0700

    Add files via upload

---
+++ Angkringan 3/ANGKRINGAN-99-KETIGA-main/ANGKRINGAN-99-KETIGA-main/angkringan-99/src/main/java/com/example/angkringan_99/Angkringan99Application.java
@@ -0,0 +1,13 @@
+package com.example.angkringan_99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angkringan99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angkringan99Application.class, args);
+	}
+
+}
---
+++ Angkringan 3/ANGKRINGAN-99-KETIGA-main/ANGKRINGAN-99-KETIGA-main/angkringan-99/src/main/java/com/example/angkringan_99/service/MenuService.java
@@ -0,0 +1,34 @@
+package com.example.angkringan_99.service;
+
+import com.example.angkringan_99.model.MenuItem;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MenuService {
+    public static List<MenuItem> getAllMenus() {
+        List<MenuItem> menu = new ArrayList<>();
+        menu.add(new MenuItem("Nasi Goreng Seafood", "Rasa gurih dengan bumbu spesial", 20000, 4.8, "Makanan Utama"));
+        menu.add(new MenuItem("Rendang Sapi", "Lembut kaya rempah", 35000, 5.0, "Makanan Utama"));
+        return menu;
+    }
+
+    public boolean login(String username, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    public boolean register(String username, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+
+    public void saveMenu(MenuItem menu) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveMenu'");
+    }
+}
+
